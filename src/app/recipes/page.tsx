65441a1d1ff022f801e70e810c6f28291b262b0e
Fewdev1122 <phumiphatasunee478@gmail.com>
2026-02-06 20:08:05 +0700

    add recipes page

---
+++ src/app/recipes/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '@/components/navV2'
+import { fredoka } from "@/lib/fonts"
+function RecipesPage() {
+  return (
+    <div className={`${fredoka.className}`}>
+      <Navbar/>
+      RecipesPage
+    </div>
+  )
+}
+
+export default RecipesPage
