88910cae43acc39822f6b005d0994fa76edc1789
Nattapon Aonsri <nopiuoov520@gmail.com>
2026-03-01 20:07:39 +0700

    fix: remotePatterns for external images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@
 /** @type {import('next').NextConfig} */
 
 /** @type {import('next').NextConfig} */
+
+/*
 const nextConfig = {
   images: {
     domains: [
@@ -12,6 +14,41 @@ const nextConfig = {
       'www.r-haan.com',
       'img-global.cpcdn.com',
       'www.ofm.co.th',
+    ],
+  },
+};
+
+module.exports = nextConfig;
+*/
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.r-haan.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'food.mthai.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.wongnai.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api2.krua.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img-global.cpcdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.ofm.co.th',
+      },
     ],
   },
 };
