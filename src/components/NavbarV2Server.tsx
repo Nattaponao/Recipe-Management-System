06634882498941a8266d1ae1141f209416c2ea6e
Nattapon Aonsri <nopiuoov520@gmail.com>
2026-03-04 14:35:50 +0700

    fix: disable cache on navbar to reflect auth state

--- src/components/NavbarV2Server.tsx
+++ src/components/NavbarV2Server.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { unstable_noStore as noStore } from 'next/cache';
 import jwt from 'jsonwebtoken';
 import { cookies } from 'next/headers';
 import NavbarV2 from '@/components/navV2';
 import { isAdminByEmail } from '@/lib/admin';
 
 export default async function NavbarV2Server() {
+  noStore();
   const cookieStore = await cookies();
   const token = cookieStore.getAll().find((c) => c.name === 'token')?.value;
 
