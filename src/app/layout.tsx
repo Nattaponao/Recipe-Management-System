3735b28f6982276918651c020189fdd9e5285133
Nattapon Aonsri <nopiuoov520@gmail.com>
2026-03-04 20:45:06 +0700

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,30 @@
-import "./globals.css"
-import { Fredoka, Kanit } from "next/font/google"
+import './globals.css';
+import { Fredoka, Kanit } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const fredoka = Fredoka({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-fredoka",
-})
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-fredoka',
+});
 
 const kanit = Kanit({
-  subsets: ["thai", "latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-kanit",
-})
+  subsets: ['thai', 'latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-kanit',
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="th"
-      className={`${fredoka.variable} ${kanit.variable}`}
-    >
-      <body>{children}</body>
+    <html lang="th" className={`${fredoka.variable} ${kanit.variable}`}>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
-  )
+  );
 }
